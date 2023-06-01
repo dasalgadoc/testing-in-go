18e77bfc2106fa36eedc2e99d5140351c6a73178
Diego Salgado <dasalgadoc@unal.edu.co>
2023-05-31 23:25:18 -0500

    01 TDD: write a test

--- 01-basics/fizzbus_test.go
+++ 01-basics/fizzbus_test.go
@@ -8,3 +8,9 @@ import (
 func Test_All_Starts_Well(t *testing.T) {
 	assert.True(t, true, "True is true!")
 }
+
+func Test_Any_Number_Should_Return_Itself(t *testing.T) {
+	assert.Equal(t, 1, FizzBuzz(1))
+	assert.Equal(t, 2, FizzBuzz(2))
+	assert.Equal(t, 4, FizzBuzz(4))
+}
