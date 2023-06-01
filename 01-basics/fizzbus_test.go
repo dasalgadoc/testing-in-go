82aafaefa9c2d41381b0cff7d852638f664d80c6
Diego Salgado <dasalgadoc@unal.edu.co>
2023-05-31 23:28:03 -0500

    03 TDD: write a test

--- 01-basics/fizzbus_test.go
+++ 01-basics/fizzbus_test.go
@@ -10,7 +10,13 @@ func Test_All_Starts_Well(t *testing.T) {
 }
 
 func Test_Any_Number_Should_Return_Itself(t *testing.T) {
-	assert.Equal(t, 1, FizzBuzz(1))
-	assert.Equal(t, 2, FizzBuzz(2))
-	assert.Equal(t, 4, FizzBuzz(4))
+	assert.Equal(t, "1", FizzBuzz(1))
+	assert.Equal(t, "2", FizzBuzz(2))
+	assert.Equal(t, "4", FizzBuzz(4))
+}
+
+func Test_Return_Fizz_If_Divisible_By_3(t *testing.T) {
+	assert.Equal(t, "Fizz", FizzBuzz(3))
+	assert.Equal(t, "Fizz", FizzBuzz(6))
+	assert.Equal(t, "Fizz", FizzBuzz(9))
 }
